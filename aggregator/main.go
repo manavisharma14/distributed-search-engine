1ce7138c2b107b3ec8bbf66385e37ea68f2ac8e1
manavisharma14 <manavisharma14@gmail.com>
2026-06-13 17:14:41 -0500

    fetchShard

--- aggregator/main.go
+++ aggregator/main.go
@@ -10,3 +10,22 @@ type SearchResult struct {
 	ID    string  `json:"ID"`
 	Score float64 `json:"Score"`
 }
+
+func fetchShard(url string) []SearchResult {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("error calling shard:", err)
+		return nil
+	}
+	defer resp.Body.Close()
+
+	var results []SearchResult
+
+	err = json.NewDecoder(resp.Body).Decode(&results)
+	if err != nil {
+		fmt.Println("decode error:", err)
+		return nil
+	}
+
+	return results
+}
